581c7fa790143e2eec3466579d5b9fba42227b11
Gerard del castillo <gerarddelcastillo@gmail.com>
2018-09-30 13:24:29 +0200

    change migration to rename instead of remove

--- hardware/migrations/0002_auto_20180923_1311.py
+++ hardware/migrations/0002_auto_20180923_1311.py
@@ -15,44 +15,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Borrowing',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('picked_up_time', models.DateTimeField(auto_now_add=True)),
-                ('return_time', models.DateTimeField(blank=True, null=True)),
-                ('borrowing_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='hardware_admin_borrowing', to=settings.AUTH_USER_MODEL)),
-                ('item', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='hardware.Item')),
-                ('return_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='hardware_admin_return', to=settings.AUTH_USER_MODEL)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.RemoveField(
-            model_name='lending',
-            name='item',
-        ),
-        migrations.RemoveField(
-            model_name='lending',
-            name='lending_by',
-        ),
-        migrations.RemoveField(
-            model_name='lending',
-            name='return_by',
-        ),
-        migrations.RemoveField(
-            model_name='lending',
-            name='user',
-        ),
-        migrations.RemoveField(
-            model_name='request',
-            name='lending',
-        ),
-        migrations.DeleteModel(
-            name='Lending',
-        ),
-        migrations.AddField(
-            model_name='request',
-            name='borrowing',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='hardware.Borrowing'),
-        ),
+        migrations.RenameField( 'Lending', 'lending_by', 'borrowing_by')
+        migrations.RenameModel( 'Lending', 'Borrowing'),
     ]
